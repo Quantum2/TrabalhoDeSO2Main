107ab5de7c0800524e8aedd66f7c107afba91b03
Rafael Francisco <rafaelfrancisco6@gmail.com>
2016-06-15 20:08:56 +0100

    Caminho do mostro conforme enunciado

--- TrabalhoSO2Monstro/main.cpp
+++ TrabalhoSO2Monstro/main.cpp
@@ -10,10 +10,13 @@
 #define BUF_SIZE 1024
 TCHAR szName[] = TEXT("Local\\MyFileMappingObject");
 
+int nSteps;
+
 void startMonstro(LPCTSTR pBuf) {
 	Monstro m;
 	MemoryShare *share;
 	int id;
+	int stepsDados = 0;
 
 	HANDLE ghWriteEvent = CreateEvent(
 		NULL,               // default security attributes
@@ -34,34 +37,96 @@ void startMonstro(LPCTSTR pBuf) {
 	while (true)
 	{
 		int r = rand() % 100;
-		if (r < 25)
+		string dir = "C";
+
+		if (r < 75 && stepsDados >= nSteps)
 		{
-			share->monstros[id].setX(share->monstros[id].getPosX() + 1);
-			cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+			r = rand() % 100;
+			stepsDados = 0;
+			if (r < 25)
+			{
+				share->monstros[id].setX(share->monstros[id].getPosX() + 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+				dir = "D";
+			}
+			if (r > 25 && r < 50)
+			{
+				share->monstros[id].setX(share->monstros[id].getPosX() - 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+				dir = "E";
+			}
+			if (r > 50 && r < 75)
+			{
+				share->monstros[id].setY(share->monstros[id].getPosY() - 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+				dir = "C";
+			}
+			if (r > 75)
+			{
+				share->monstros[id].setY(share->monstros[id].getPosY() + 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+				dir = "B";
+			}
 		}
-		if (r > 25 && r < 50)
+		else
+		{
+			if (dir == "D")
+			{
+				share->monstros[id].setX(share->monstros[id].getPosX() + 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+			}
+			if (dir == "B")
+			{
+				share->monstros[id].setY(share->monstros[id].getPosY() + 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+			}
+			if (dir == "E")
+			{
+				share->monstros[id].setX(share->monstros[id].getPosX() - 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+			}
+			if (dir == "C")
+			{
+				share->monstros[id].setY(share->monstros[id].getPosY() - 1);
+				cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+			}
+			stepsDados++;
+		}
+
+		if (share->monstros[id].getPosX() >= share->tamanhoMapa - 1)
 		{
 			share->monstros[id].setX(share->monstros[id].getPosX() - 1);
-			cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
 		}
-		if (r > 50 && r < 75)
+		if (share->monstros[id].getPosX() <= 0)
 		{
-			share->monstros[id].setY(share->monstros[id].getPosY() - 1);
-			cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
+			share->monstros[id].setX(share->monstros[id].getPosX() + 1);
 		}
-		if (r > 75)
+		if (share->monstros[id].getPosY() <= 0)
 		{
 			share->monstros[id].setY(share->monstros[id].getPosY() + 1);
-			cout << "Monstro com ID " << id << " anda para X:" << share->monstros[id].getPosX() << " Y:" << share->monstros[id].getPosY() << endl;
 		}
+		if (share->monstros[id].getPosY() >= share->tamanhoMapa - 1)
+		{
+			share->monstros[id].setY(share->monstros[id].getPosY() - 1);
+		}
+
 		SetEvent(ghWriteEvent);
 		_sleep(1500);
 	}
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	HANDLE hMapFile;
 	LPCTSTR pBuf;
+
+	if (argc == 1)
+	{
+		nSteps = atoi(argv[0]);
+	}
+	else
+	{
+		nSteps = 3;
+	}
 
 	hMapFile = OpenFileMapping(
 		FILE_MAP_ALL_ACCESS,   // read/write access
